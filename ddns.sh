97255772726ab4971223ec707efe0df7e6f7fb35
jinnang233 <77971207+jinnang233@users.noreply.github.com>
2023-12-20 18:15:05 +0800

    Add files via upload

--- ddns.sh
+++ ddns.sh
@@ -11,8 +11,9 @@ fi
 # 获取IP地址并回显
 IP6=`ip -6 addr show | grep global | awk '{print \$2}' | awk -F "/" '{print \$1}'`
 for IP in ${IP_range[@]}; do
-        echo "Local IP: $IP"
-	IP6=$(echo $IP6 |grep -v $(echo $IP |awk -F"/" '{print $1}'))
+	IP=$(echo "$IP" |awk -F"/" '{print $1}')
+	echo "Local IP: $IP"
+	IP6=$(echo $IP6 |sed "s/$IP//g")
 done
 echo  -e "All IP Addresses: \n$IP6"
 
